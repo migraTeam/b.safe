6f36a5a9bf630222a2ffab20f190ef63c4501fad
Ion Oaie <ion.oaie@gmail.com>
2020-06-28 13:22:26 +0200

    Updates

---
+++ precache-manifest.8b1fe7ae98cc1b2d4857ed01a9a110d0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14cd9a631a6779f7ea73303bcdb93865",
+    "url": "/b.safe/index.html"
+  },
+  {
+    "revision": "0683e7620325c22cfe13",
+    "url": "/b.safe/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "184e72f4b5c873662238",
+    "url": "/b.safe/static/js/2.a7b38e48.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/b.safe/static/js/2.a7b38e48.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0683e7620325c22cfe13",
+    "url": "/b.safe/static/js/main.ff418e78.chunk.js"
+  },
+  {
+    "revision": "683f1ce6dd7603a03c72",
+    "url": "/b.safe/static/js/runtime-main.169f2a19.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/b.safe/static/media/logo.5d5d9eef.svg"
+  }
+]);
