5c5cbbcd47bfaaa692a01ee6ade1e8448a4f1b7f
Ion Oaie <ion.oaie@gmail.com>
2020-06-29 12:41:09 +0200

    mobile menu

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,26 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ButtonCall from './ButtonCall'
 import flages from '../images/flages.png';
-import hicon from '../images/hicon.png';
+import menu from '../images/menu.png';
 import close from '../images/close.png';
 import Logo from '../images/logo.png'
 import '../App.css';
 
-function Header() {
+const Header = () => {
+    const [mobileMenu, setmobileMenu] = useState("0");
+
+        const mobMenuOpen = () => {
+          if(mobileMenu == 0){
+        setmobileMenu("1")
+          }
+          else{
+            setmobileMenu("0")
+          }
+    };
 
   return (
 <header>
         <div className="topnav">
          <img src={Logo} className="flag" alt="Logo" />
          <ButtonCall />
-           <div id="centermobmenu">
-               <img src={hicon} className="flag" alt="hicon" />
+           <div id="mobileMenu">
+           {mobileMenu == 0 ? <>   
+               <img onClick={mobMenuOpen} src={menu} className="flag" alt="open mobile menu" />
+               </> : null}
+              {mobileMenu == 1 ? <>        
                <div className="myLinks">
-                 <img src={close} className="flag" alt="close" />
+                 <img onClick={mobMenuOpen} src={close} className="flag" alt="close mobile menu" />
                  <a href="Archive.html">Archive</a>
                  <a href="About.html">About</a>
                  <img src={flages} className="flag" alt="logo" />
              </div>
+             </> : null}
            </div>
         </div>
 </header>
