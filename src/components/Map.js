f36135534a2b4466471fc3ed058e9009236b7dae
Ion Oaie <ion.oaie@gmail.com>
2020-06-30 09:14:09 +0200

    map with key

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,14 +1,24 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import map from '../images/map.png';
 import '../App.css';
 
 function Map() {
+
+var x = document.getElementById("mapholder");
+
+  useEffect(() => {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition);
+
+  } else { 
+    x.innerHTML = "Geolocation is not supported by this browser.";
+  }
+  });
+
 function showPosition(position) {
   var latlon = position.coords.latitude + "," + position.coords.longitude;
-
-  var img_url = `https://maps.googleapis.com/maps/api/staticmap?center=
-  "+latlon+"&zoom=14&size=400x300&sensor=false&key=AIzaSyB27IflEAH4CLf825AvmiMbXE7NTK9obeA`;
-
+      console.log(latlon)
+  var img_url = `https://maps.googleapis.com/maps/api/staticmap?center=${latlon}&zoom=14&size=400x300&sensor=false&key=AIzaSyCGpc8kGF95sQU2Ky8lf3YHl5Y-E8-eRoM`;
   document.getElementById("mapholder").innerHTML = "<img src='"+img_url+"'>";
 }
 
