1bc6d339cec62027c7e3e252207932c2f9a96a1b
Ion Oaie <ion.oaie@gmail.com>
2020-06-30 08:36:37 +0200

    map

--- src/components/Map.js
+++ src/components/Map.js
@@ -7,15 +7,16 @@ function showPosition(position) {
   var latlon = position.coords.latitude + "," + position.coords.longitude;
 
   var img_url = `https://maps.googleapis.com/maps/api/staticmap?center=
-  "+latlon+"&zoom=14&size=400x300&sensor=false&key=YOUR_KEY`;
+  "+latlon+"&zoom=14&size=400x300&sensor=false&key=AIzaSyB27IflEAH4CLf825AvmiMbXE7NTK9obeA`;
 
   document.getElementById("mapholder").innerHTML = "<img src='"+img_url+"'>";
 }
 
   return (
-
-  <div className="MainContainer" id="mapholder"><img src={map} className="MainImage" alt="map" /></div>
-
+<div>
+  <div className="MainContainer" id=""><img src={map} className="MainImage" alt="map" /></div>
+  <div className="MainContainer" id="mapholder"></div>
+</div>
   );
 }
 export default Map;
